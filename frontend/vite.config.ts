6a10bcd3b6bec5cfb629b3f158493a550e1af5c7
mrattez <mrattez@student.42nice.fr>
2023-10-05 16:44:41 +0200

    ✨ WORKING

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,5 +2,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		proxy: {
+			'^/api/': {
+				target: 'http://backend:3000',
+				changeOrigin: false,
+				secure: false,
+				ws: false,
+			}
+		}
+	}
 });
