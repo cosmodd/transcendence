09d75de007026507da8fd50d50e5db3e9a09007a
Maxim <lolesqua@gmail.com>
2023-11-27 12:30:56 +0100

    🔄️ Updated core of the project

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for transcendence project.
+URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
