4ffb8b7d22d8ea3fa89155479867bf14ae844877
dcdnce <pforesti@student.42nice.fr>
2024-02-02 17:24:48 +0100

    🏗️ Pong accessible via nginx Solution TEMPORAIRE pours les fichiers statics venant de django. Version finale -> collecter dans le frontend.

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 	# path('api/', include('example_api.urls')),
 	path('api/', include('users.urls')),
+    path('pong/', include('pong.urls')),
 ]
