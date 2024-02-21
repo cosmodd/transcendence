32bf920326d4a285b2c6973ce87093b03f676663
mzaraa <mahmoud.zaraa1@gmail.com>
2024-02-21 15:33:33 +0100

    password not hashed, but basic register/login/profile endpoint work

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Account
+import sys
 
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,6 +9,30 @@ class AccountSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'password': {'write_only': True}
         }
+
+def check_password(raw_password, password):
+    return raw_password == password
+
+class LoginSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=30, required=True)
+    password = serializers.CharField(max_length=30, required=True, write_only=True)
+
+    class Meta:
+        model = Account
+        fields = ['username', 'password']
+
+
+    def validate(self, data):
+        user = Account.objects.get(username=data['username'])
+        # print(data['password'], file=sys.stderr)
+        if not user:
+            raise serializers.ValidationError("Invalid username")
+        if not check_password(data['password'], user.password):
+            raise serializers.ValidationError("Invalid password")
+        print("PASSED", file=sys.stderr)
+        return data
+
+            
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/users/views.py
+++ backend/users/views.py
@@ -2,7 +2,9 @@ from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .models import Account
-from .serializers import AccountSerializer, ProfileSerializer
+from .serializers import AccountSerializer, ProfileSerializer, LoginSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+import sys
 
 class RegisterView(generics.CreateAPIView):
     queryset = Account.objects.all()
@@ -11,16 +13,31 @@ class RegisterView(generics.CreateAPIView):
 
 # custom error message for login if credentials are invalid
 class LoginView(TokenObtainPairView):
-    serializer_class = AccountSerializer
+    serializer_class = LoginSerializer
+    permission_classes = [permissions.AllowAny]
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        try:
-            serializer.is_valid(raise_exception=True)
-        except:
-            return Response({'error_message': 'Invalid username or password'}, status=400)
-        else:
-            return Response(serializer.validated_data, status=200)
+        # print(request.data, file=sys.stderr)
+        serializer.is_valid(raise_exception=True)
+        # print(serializer.validated_data, file=sys.stderr)
+        print("HERE", file=sys.stderr)
+        user = Account.objects.get(username=request.data['username'])
+        refresh = RefreshToken.for_user(user)
+        return Response(
+        {
+
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+            "user": {
+                "id": user.id,
+                "username": user.username,
+                "email": user.email,
+                "profile_image": user.profile_image.url if user.profile_image else None
+            }
+        }
+        )
+
 
 class ProfileView(generics.RetrieveAPIView):
     serializer_class = ProfileSerializer
