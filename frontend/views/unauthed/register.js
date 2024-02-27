139fc23f8e87b21e3ee8078d0588708ce28a65fa
cosmodd <lolesqua@gmail.com>
2024-02-27 10:53:14 +0100

    ✅ Added default value to registration form for testing purposes

--- frontend/views/unauthed/register.js
+++ frontend/views/unauthed/register.js
@@ -8,13 +8,13 @@ export default (container) => {
 				<form class="card p-5 d-flex flex-column gap-3" id="register-form" style="min-width: 400px;">
 					<h1 class="h3 fw-normal">Register</h1>
 					<label for="username" class="visually-hidden">Username</label>
-					<input type="text" id="username" class="form-control" placeholder="Username" required autofocus />
+					<input type="text" id="username" class="form-control" placeholder="Username" value="username" required autofocus />
 					<label for="email" class="visually-hidden">Email</label>
-					<input type="email" id="email" class="form-control" placeholder="mail@domain.ext" required />
+					<input type="email" id="email" class="form-control" placeholder="mail@domain.ext" value="username@user.com" required />
 					<label for="password" class="visually-hidden">Password</label>
-					<input type="password" id="password" class="form-control" placeholder="Password" required />
+					<input type="password" id="password" class="form-control" placeholder="Password" value="password" required />
 					<label for="password-confirm" class="visually-hidden">Confirm Password</label>
-					<input type="password" id="password-confirm" class="form-control" placeholder="Confirm Password" required />
+					<input type="password" id="password-confirm" class="form-control" placeholder="Confirm Password" value="password" required />
 					<p class="mb-0 text-muted">Already have an account? <a href="/login">Login</a></p>
 					<div class="d-flex flex-column justify-content-center gap-2">
 						<button class="btn btn-primary" type="submit">Register</button>
