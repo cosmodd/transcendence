9de274cccad4f9b74d67250c72375545f21fb8d1
Maxim <lolesqua@gmail.com>
2024-04-04 11:30:42 +0200

    🎨 Bracket centered on page

--- frontend/src/lib/components/tournament/Matchup.svelte
+++ frontend/src/lib/components/tournament/Matchup.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 </script>
 
-<div class="card d-flex flex-column border-2">
+<div class="card d-flex flex-column border-2 overflow-hidden">
 	<div class="player p-2 d-flex flex-row gap-2 align-items-center">
 		<img
 			src="https://via.placeholder.com/128"
@@ -9,7 +9,7 @@
 			alt="avatar"
 			style="width: 24px; height: 24px;"
 		/>
-		<span class="fw-bold me-1">Cosmo</span>
+		<span class="displayname fw-bold me-1">Cosmo</span>
 		<span class="score badge bg-success ms-auto">5</span>
 	</div>
 	<hr class="m-0 border-2" />
@@ -20,7 +20,13 @@
 			alt="avatar"
 			style="width: 24px; height: 24px;"
 		/>
-		<span class="fw-bold me-1">SomethingIsCUMING</span>
+		<span class="text-truncate fw-bold me-1">WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW</span>
 		<span class="score badge bg-danger ms-auto">0</span>
 	</div>
 </div>
+
+<style>
+	.player span:first-of-type {
+		max-width: 200px;
+	}
+</style>
--- frontend/src/routes/(authed)/tournaments/test/+page.svelte
+++ frontend/src/routes/(authed)/tournaments/test/+page.svelte
@@ -1,53 +1,51 @@
 <script lang="ts">
 	import Matchup from "$lib/components/tournament/Matchup.svelte";
-	import Matchups from "$lib/components/tournament/Matchups.svelte";
+
+	const playerCount: number = 2 << 2;
 </script>
 
-<div class="bracket d-flex flex-row h-100">
-	<div class="round quarter d-flex flex-column justify-content-between h-100">
-		{#each Array(4) as i}
-			<div class="matchup flex-grow-1 d-flex align-items-center">
-				<Matchup />
-			</div>
-		{/each}
-	</div>
-	<div class="quarter connectors d-flex flex-column">
-		{#each Array(4) as i}
-			<div class="connector flex-grow-1 position-relative" />
-		{/each}
-	</div>
+<div class="bracket d-flex flex-row justify-content-center h-100">
+	{#if playerCount >= 8}
+		<div class="round quarter d-flex flex-column justify-content-between">
+			{#each Array(4) as i}
+				<div class="matchup flex-grow-1 d-flex align-items-center py-2">
+					<Matchup />
+				</div>
+			{/each}
+		</div>
+		<div class="quarter connectors d-flex flex-column">
+			{#each Array(4) as i}
+				<div class="connector flex-grow-1 position-relative" />
+			{/each}
+		</div>
+	{/if}
 
-	<div class="round semi d-flex flex-column justify-content-between h-100">
-		{#each Array(2) as i}
-			<div class="matchup flex-grow-1 d-flex align-items-center">
-				<Matchup />
-			</div>
-		{/each}
-	</div>
-	<div class="semi connectors d-flex flex-column">
-		{#each Array(2) as i}
-			<div class="connector flex-grow-1 position-relative" />
-		{/each}
-	</div>
+	{#if playerCount >= 4}
+		<div class="round semi d-flex flex-column justify-content-between">
+			{#each Array(2) as i}
+				<div class="matchup flex-grow-1 d-flex align-items-center py-2">
+					<Matchup />
+				</div>
+			{/each}
+		</div>
+		<div class="semi connectors d-flex flex-column">
+			{#each Array(2) as i}
+				<div class="connector flex-grow-1 position-relative" />
+			{/each}
+		</div>
+	{/if}
 
-	<div class="round final d-flex flex-column justify-content-between h-100">
-		<div class="matchup flex-grow-1 d-flex align-items-center">
+	<div class="round final d-flex flex-column justify-content-between">
+		<div class="matchup flex-grow-1 d-flex align-items-center py-2">
 			<Matchup />
 		</div>
 	</div>
-	<!-- <div class="round semi d-flex flex-column justify-content-center">
-		<Matchup />
-		<Matchup />
-	</div>
-	<div class="round final d-flex flex-column justify-content-center">
-		<Matchup />
-	</div> -->
 </div>
 
 <style>
 	.connectors {
 		--connector-width: 2px;
-		min-width: 50px;
+		min-width: 4rem;
 	}
 
 	.connector::before {
