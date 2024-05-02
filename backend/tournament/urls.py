851619f14485074c9aeaf4ab38cc89537d2ddab4
Paul Forestie <pforesti@student.42nice.fr>
2024-05-02 14:20:34 +0200

    🎨 Renamed tournament apis

--- backend/tournament/urls.py
+++ backend/tournament/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-	path("tournament/fill/", FillTournamentView.as_view(), name='fill-tournament'),
+	path("tournament/join/", FillTournamentView.as_view(), name='fill-tournament'),
 	path("tournament/create/", CreateTournamentView.as_view(), name='create-tournament'),
-	path("tournament/list/", ActiveTournamentsListView.as_view(), name='list-tournaments'),
-	path("tournament/get/<str:pk>/", SpecificTournamentView.as_view(), name='get-tournament')
+	path("tournament/", ActiveTournamentsListView.as_view(), name='list-tournaments'),
+	path("tournament/<str:pk>/", SpecificTournamentView.as_view(), name='get-tournament')
 ]
