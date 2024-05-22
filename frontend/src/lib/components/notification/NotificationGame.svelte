d2b0d231ad10d713983ec701d4d05e13ca142af7
mzaraa <mahmoud.zaraa1@gmail.com>
2024-05-22 21:02:47 +0200

    notif_tournament

--- frontend/src/lib/components/notification/NotificationGame.svelte
+++ frontend/src/lib/components/notification/NotificationGame.svelte
@@ -2,11 +2,18 @@
     let visible: boolean = false;
     let message: string = "";
     let link: string = "/game/online";
+    let data: any;
   
     function handleNotification(e) {
-      const det = e.detail;
-      message = "Join the game!";
-      visible = true;
+      data = e.detail;
+      if (data.sender === "system") {
+        message = data.message;
+        visible = true;
+      }
+      else {
+        message = "Join the game!";
+        visible = true;
+      }
   
       setTimeout(() => {
         visible = false;
@@ -17,11 +24,16 @@
       visible = false;
       window.location.href = link;
     };
+
+    const joinTournament = () => {
+      visible = false;
+      window.location.href = link;
+    };
   </script>
   
   <svelte:window on:notification={handleNotification} />
   
-  {#if visible}
+  {#if visible && message === "Join the game!"}
     <div class="toast show position-fixed bottom-0 end-0 p-3" style="z-index: 11">
       <div class="toast-header">
         <strong class="me-auto">Notification</strong>
@@ -32,6 +44,19 @@
         <div class="mt-2 pt-2 border-top">
           <button type="button" class="btn btn-primary btn-sm" on:click={joinGame}>Join Game</button>
         </div>
+      </div>
+    </div>
+  {:else if visible && data.sender === "system"}
+    <div class="toast show position-fixed bottom-0 end-0 p-3" style="z-index: 11">
+      <div class="toast-header">
+        <strong class="me-auto">Notification</strong>
+        <button type="button" class="btn-close" on:click={() => (visible = false)}></button>
+      </div>
+      <div class="toast-body">
+        {message}
+      </div>
+      <div class="mt-2 pt-2 border-top">
+        <button type="button" class="btn btn-primary btn-sm" on:click={() => (visible = false)}>Join tournament</button>
       </div>
     </div>
   {/if}
