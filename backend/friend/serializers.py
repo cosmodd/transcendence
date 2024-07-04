0deaa2542851adea252af3857ed3327980b39bb1
Mahmoud Zaraa <mzaraa@c2r6p4.42nice.fr>
2024-07-04 14:05:00 +0200

    friendship_system_little_fix

--- backend/friend/serializers.py
+++ backend/friend/serializers.py
@@ -7,12 +7,18 @@ class FriendSerializer(serializers.ModelSerializer):
         fields = ['id', 'user', 'friend', 'created_at']
 
 class FriendRequestSerializer(serializers.ModelSerializer):
-    from_user = serializers.StringRelatedField()
-    to_user = serializers.StringRelatedField()
+    from_user = serializers.SerializerMethodField()
+    to_user = serializers.SerializerMethodField()
 
     class Meta:
         model = FriendRequest
         fields = ['id', 'from_user', 'to_user', 'created_at']
+
+    def get_from_user(self, obj):
+        return obj.from_user.username
+    
+    def get_to_user(self, obj):
+        return obj.to_user.username
 
 
 class BlockSerializer(serializers.ModelSerializer):
