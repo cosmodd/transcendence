371a08fed284dba7d6fb2c94ede0b2f27e9c70f0
Mahmoud Zaraa <mzaraa@c2r6p4.42nice.fr>
2024-07-05 15:07:44 +0200

    Update settings.py and add template.env file

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x67ihse3*73t@zv9m582zc4_s(!p0w!7c+7#xo03x$qetv=vj3'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
