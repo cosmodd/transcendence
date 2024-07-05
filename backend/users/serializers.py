67e2feaf67ceead35e841d71f60b81e9b2e1df09
cosmodd <lolesqua@gmail.com>
2024-07-05 20:30:23 +0200

    Display name needs to be between 2 and 32 chars

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -66,8 +66,12 @@ class UpdateProfileSerializer(serializers.ModelSerializer):
     def validate(self, data):
         user = self.context['request'].user
 
-        if 'display_name' in data and not re.match("^[a-zA-Z0-9_.]{8,32}$", data['display_name']):
-            raise serializers.ValidationError({"display_name": "Display name can only contain alphanumeric characters, underscores and dots"})
+        if 'display_name' in data:
+            if len(data['display_name']) < 2 or len(data['display_name']) > 32:
+                raise serializers.ValidationError({"display_name": "must be between 2 and 32 characters long"})
+
+            if not re.match("^[a-zA-Z0-9_.]$", data['display_name']):
+                raise serializers.ValidationError({"display_name": "can only contain alphanumeric characters, underscores and dots"})
 
         # Check if field old_password and password are present
         if 'old_password' in data and 'password' in data:
