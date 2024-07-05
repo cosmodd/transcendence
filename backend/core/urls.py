adb83d56e913a88c3ff67de8fd20a80c9ced2598
Maxim <lolesqua@gmail.com>
2024-07-05 16:34:08 +0200

    Django serves static files in debug=false

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,6 +29,7 @@ urlpatterns = [
     path('api/', include('chat.urls')),
     path('api/', include('tournament.urls')),
     path('api/', include('friend.urls')),
+	re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
